0a1d0dc57d4e7f8e7396bbb8506bd4e271ce4746
qiulin <qlin.dev@outlook.com>
2025-03-22 23:57:04 +0800

    完善编辑器页面

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,5 @@
 import {View} from "@tarojs/components";
-import {Cell, CellGroup, Col, Grid, GridItem, Icon, Image, Row} from "@antmjs/vantui";
+import {Cell, CellGroup, Col, Grid, GridItem, Icon, Image, Row, Space} from "@antmjs/vantui";
 import {useUserStore} from "src/stores/user-store";
 import {useEffect} from "react";
 import Taro from "@tarojs/taro";
@@ -47,31 +47,27 @@ const Profile = () => {
           <Row className='m-2 flex items-center'>
             <Col span={4}>
               <Image src={user?.avatarUrl ?? ""} round width={100} height={100}
-                className='flex items-center justify-center'
+                     className='flex items-center justify-center'
               />
             </Col>
             <Col span={20}>
               <View className='flex items-center'>
-                <Row>
-                  <Col span={20}>
-                    <View>{user?.displayName}</View>
-                  </Col>
-                  <Col span={2}>
-                    <Icon name='replay' onClick={handleUpdateProfile} />
-                  </Col>
-                </Row>
+                <Space>
+                  <View>{user?.displayName}</View>
+                  <Icon name='miniprogram-o' onClick={handleUpdateProfile}></Icon>
+                </Space>
               </View>
             </Col>
           </Row>
           <Grid columnNum={3}>
-            <GridItem icon='smile-o' text='1' />
-            <GridItem icon='fire-o' text='29' />
-            <GridItem icon='award-o' text='3' />
+            <GridItem icon='smile-o' text='1' onClick={() => Taro.navigateTo({url: '/pages/student/index'})}/>
+            <GridItem icon='fire-o' text='29' onClick={() => Taro.navigateTo({url: '/pages/content/index'})}/>
+            <GridItem icon='award-o' text='3' onClick={() => Taro.navigateTo({url: '/pages/class/index'})}/>
           </Grid>
         </View>
         <View className='mt-6 rounded-md'>
           <CellGroup>
-            <Cell title='退出登录' onClick={handleLogout} />
+            <Cell title='退出登录' onClick={handleLogout}/>
           </CellGroup>
         </View>
       </View>
